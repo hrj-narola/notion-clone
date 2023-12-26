6543ef3305e213bbd84243bb0fc679204df63d82
hrj-narola <hrj@narola.email>
2023-12-26 16:45:24 +0530

    initial commit

---
+++ app/(marketing)/_components/heroes.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+
+export const Heroes =()=>{
+    return(
+        <div className="flex flex-col items-center justify-center max-w-5xl ">
+            <div className="flex itmes-center">
+                <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]">
+                    <Image src={"/documents.png"} fill className="object-contain dark:hidden" alt="document" />
+                    <Image src={"/documents-dark.png"} fill className="object-contain hidden dark:block" alt="document" />
+                </div>
+                <div className="relative h-[400px] w-[400px] hidden md:block">
+                <Image src={"/reading.png"} fill className="object-contain dark:hidden" alt="reading" />
+                <Image src={"/reading-dark.png"} fill className="object-contain hidden dark:block" alt="reading" />
+                </div>
+            </div>
+        </div>
+    )
+}
